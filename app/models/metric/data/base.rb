56f7cb4730cc7d4768850369ce8b1e75eec4ca7a
Ivan Kornilov <vano468@gmail.com>
2016-01-27 22:25:10 +0300

    Refactor Metric::Data:Base, add MessagesCountBetweenUsers metric data

--- app/models/metric/data/base.rb
+++ app/models/metric/data/base.rb
@@ -23,17 +23,22 @@ class Metric::Data::Base
   def set_attributes
     allowed_attributes.each do |attr, settings|
       if incoming_attributes[attr]
-        set_attribute attr, settings
-      else
-        raise Metric::Data::AttributeMissed, "#{attr} attribute is missed" unless settings[:optional]
+        set_attribute attr, attribute_value(attr)
+      elsif settings.key? :default
+        set_attribute attr, settings[:default]
+      elsif !settings[:optional]
+        raise Metric::Data::AttributeMissed, "#{attr} attribute is missed"
       end
     end
   end
 
-  def set_attribute(attr, settings)
-    value = settings[:transform].kind_of?(Proc) ?
-      settings[:transform].call(incoming_attributes[attr]) : incoming_attributes[attr]
+  def set_attribute(attr, value)
     self.instance_variable_set :"@#{attr}", value
     self.class_eval { attr_reader attr }
+  end
+
+  def attribute_value(attr)
+    allowed_attributes[attr][:transform].kind_of?(Proc) ?
+      allowed_attributes[attr][:transform].call(incoming_attributes[attr]) : incoming_attributes[attr]
   end
 end
