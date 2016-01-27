a73596d05e3f42e48184d1cb0cf7bfbb45008a4f
Ivan Kornilov <vano468@gmail.com>
2016-01-27 12:27:40 +0300

    Move spec settings to shared

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,25 +1,3 @@
 ENV['RAILS_ENV'] ||= 'test'
-require 'spec_helper'
 
-reports_dir = ENV['WERCKER_REPORT_ARTIFACTS_DIR'] || File.expand_path('../../tmp', __FILE__)
-
-if ENV.key?('coverage') || ENV.key?('CI')
-  require 'simplecov'
-  SimpleCov.coverage_dir File.join(reports_dir, 'coverage')
-  SimpleCov.start :rails
-end
-
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
-RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
-  config.infer_spec_type_from_file_location!
-
-  if ENV.key?('CI')
-    config.add_formatter :html, File.join(reports_dir, 'rspec', 'rspec.html')
-  end
-end
+require_relative '../shared/spec/spec_helper'
